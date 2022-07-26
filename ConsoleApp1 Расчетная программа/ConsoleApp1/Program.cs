f7a0271508fe3a9d3f01e98d250cb5c2eba386e0
BernikovaLera <109979423+BernikovaLera@users.noreply.github.com>
2022-07-26 17:50:20 +0300

    C-Sharp

---
+++ ConsoleApp1 Расчетная программа/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Ab
+    {
+        public static int sub(int a, int b) { return a - b; }
+        public static int div(int a, int b) { return a / b; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConsoleApp1 Расчетная программа/ConsoleApp1Tests/AbTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+[TestClass()]
+    public class AbTests
+    {
+        [TestMethod()]
+        public void subTest()
+        {
+            int a = 2;
+            int b = 3;
+            int expected = -1;
+            int actual;
+            actual = Ab.sub(a, b);
+            Assert.AreEqual(expected, actual);
+            // Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void subTest1()
+        {
+            int a = 3;
+            int b = 3;
+            int expected = 0;
+            int actual;
+            actual = Ab.sub(a, b);
+            Assert.AreEqual(expected, actual);
+            // Assert.Fail();
+        }
+        [TestMethod()]
+        public void divTest()
+        {
+            int a = 9;
+            int b = 3;
+            int expected = 4;
+            int actual;
+            actual = Ab.div(a, b);
+            Assert.AreEqual(expected, actual);
+            // Assert.Fail();
+        }
+    }
+}
