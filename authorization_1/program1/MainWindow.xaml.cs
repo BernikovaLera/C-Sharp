ade20d2bb9ee93b6107ed565a9c9564e5e0a572d
BernikovaLera <109979423+BernikovaLera@users.noreply.github.com>
2022-07-27 13:19:33 +0300

    authorization_1

---
+++ authorization_1/program1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace program1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonEnret_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxLogin.Text) | string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Введите логин и пароль!");
+                return;
+            }
+                using (var db = new Entities())
+         {
+            var user = db.Table_1
+            .AsNoTracking()
+            .FirstOrDefault(u=>u. Login == TextBoxLogin.Text & u.Password==PasswordBox.Password);
+        if (user == null)
+        {
+        MessageBox.Show("Пользователь с такими данными не найден!");
+        return;
+        }
+        MessageBox.Show("Пользователь успешно найден!");
+             switch (user.Login)
+                 {
+                 case "Student":
+                     MessageBox.Show("Вы успешно вошли как студент!");
+                     Student window1 = new Student();
+                     window1.Show();
+                     break;
+                 case "Teachr":
+                     MessageBox.Show("Вы успешно вошли как учитель!");
+                     Teacher window2 = new Teacher();
+                     window2.Show();
+                     break;
+                 }
+        }
+        }
+    }
+}
---
+++ authorization_1/program1/Teacher.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using program1.Pages;
+
+namespace program1
+{
+    /// <summary>
+    /// Логика взаимодействия для Teacher.xaml
+    /// </summary>
+    public partial class Teacher : Window
+    {
+        StudentPage pST = new StudentPage();
+        TeacherPage tST = new TeacherPage();
+        public Teacher()
+        {
+            InitializeComponent();
+        }
+        private void ForwarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoForward)
+            {
+                MyFrame.GoForward();
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+            }
+        }
+
+        private void Button_Teacher(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new TeacherPage());
+        }
+
+        private void Button_Student(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(new StudentPage());
+        }
+    }
+}
